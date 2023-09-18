b680fc6faaad8b5bcd51b6b051295a91295bee6b
Vipul <vipulsingh161@gmail.com>
2023-09-18 11:01:28 +0530

    Made changes in the dependcies and in WebhookControllerTest, tests are working now

--- src/test/java/org/example/WebhookControllerTest.java
+++ src/test/java/org/example/WebhookControllerTest.java
@@ -3,12 +3,17 @@ package org.example;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,34 +22,55 @@ public class WebhookControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
     private ObjectMapper objectMapper;
 
+    @InjectMocks
+    private WebhookController webhookController;
+
     @BeforeEach
-    void setUp() {
-        objectMapper = new ObjectMapper();
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void testHandleWebhook_ValidApiKey() throws Exception {
+    public void handleWebhook_ValidApiKey_ShouldReturnOk() throws Exception {
         WebhookRequest request = new WebhookRequest();
         request.setApiKey("your-api-key");
-        request.setData("some-data");
+        request.setData("someData");
 
         mockMvc.perform(post("/webhook")
-                        .contentType("application/json")
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
-    void testHandleWebhook_InvalidApiKey() throws Exception {
+    public void handleWebhook_InvalidApiKey_ShouldReturnUnauthorized() throws Exception {
         WebhookRequest request = new WebhookRequest();
         request.setApiKey("invalid-api-key");
-        request.setData("some-data");
+        request.setData("someData");
 
         mockMvc.perform(post("/webhook")
-                        .contentType("application/json")
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isUnauthorized());
+                .andDo(print())
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.status").value(401))
+                .andExpect(jsonPath("$.message").value("Invalid API Key"));
     }
+
+    @Test
+    public void handleWebhook_MissingApiKey_ShouldReturnBadRequest() throws Exception {
+        WebhookRequest request = new WebhookRequest();
+        request.setData("someData");
+
+        mockMvc.perform(post("/webhook")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
 }
